eb5d6e6aa8a9f710b4610cef2135bbfbf6537aca
Guilherme <guilherme.silva700@fatec.sp.gov.br>
2025-08-14 09:14:55 -0300

    ajuste-no-nextline

--- 2-Semestre/3-Atividades/Atv/Multiplacao.java
+++ 2-Semestre/3-Atividades/Atv/Multiplacao.java
@@ -5,7 +5,7 @@ public class Multiplacao {
     public static void main(String[] args) {
         System.out.println("Digite o 1º número: ");
         Scanner inputDados = new Scanner(System.in);
-        float num1 = inputDados.nexLine();
+        float num1 = inputDados.nextLine();
         System.out.println("Digite o 2º número: ");
         float num2 = inputDados.nextLine();
         float mult = num1 * num2;
