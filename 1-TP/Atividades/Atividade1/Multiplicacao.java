1304249f14eaf415e3d1e23bfce6ccd119cb00a2
Guilherme <guilherme.silva700@fatec.sp.gov.br>
2025-08-15 21:59:05 -0300

    criação-de-exercicios-da-aula-de-TP

---
+++ 1-TP/Atividades/Atividade1/Multiplicacao.java
@@ -0,0 +1,24 @@
+package Atividades.Atividade1;
+import java.util.Scanner;
+
+public class Multiplicacao {
+
+    public static void main(String[] args) {
+        //Nome: Guilherme Gomes da Silva
+        //RA: 2171392511043
+        //Objetivo: Faça um programa na Linguagem Java que receba 2 números e apresente a multiplicação dos dois números.
+
+        Scanner inputDados = new Scanner(System.in);
+
+        System.out.print("Digite o 1º número: ");
+        float num1 = inputDados.nextFloat();
+
+        System.out.print("Digite o 2º número: ");
+        float num2 = inputDados.nextFloat();
+
+        float mult = num1 * num2;
+        System.out.println("O resultado da multiplicação = " + mult);
+
+        inputDados.close();
+    }
+}
---
+++ 1-TP/Atividades/Atividade2/Temperatura.java
@@ -0,0 +1,20 @@
+package Atividades.Atividade2;
+import java.util.Scanner;
+
+public class Temperatura {
+    public static void main(String[] args) {
+        //Nome: Guilherme Gomes da Silva
+        //RA: 2171392511043
+        //Objetivo: Faça um programa na Linguagem Java que leia uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit. A fórmula de conversão é F <-- (9 * C + 160) /5, sendo f a temperatura em Fahrenheit e C a temperatura em Celsius.
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Informe a temperatura (°C): ");
+        float grausC = entrada.nextFloat();
+
+        float grausF = (9 * grausC + 160) / 5;
+        System.out.println("A temperatura " + grausC + " °C convertida é igual a " + grausF + " °F");
+
+        entrada.close();
+    }
+}
