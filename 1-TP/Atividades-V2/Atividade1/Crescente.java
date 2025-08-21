0ec256759d572824f409dfd7ddf54d749bb657b7
Guilherme <guilherme.silva700@fatec.sp.gov.br>
2025-08-21 09:17:50 -0300

    criação-de-atividade-16

---
+++ 1-TP/Atividades-V2/Atividade1/Crescente.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+//package 1-TP.Atividades-V2.Atividade1;
+
+public class Crescente {
+    public static void main(String[] args) {
+        try (Scanner entrada = new Scanner(System.in)) 
+        {
+            System.out.print("Digite o 1º valor: ");
+            int valor1 = entrada.nextInt();
+            System.out.print("Digite o 2º valor: ");
+            int valor2 = entrada.nextInt();
+            System.out.print("Digite o 3º valor: ");
+            int valor3 = entrada.nextInt();
+
+        }
+    }
+}
